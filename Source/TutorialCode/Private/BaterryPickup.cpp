a080080f4f869813678d47d18d84e396d58a16d8
ekflame <ekflame@hotmail.com>
2014-08-09 21:04:34 +0900

    bateryPickup 추가

---
+++ Source/TutorialCode/Private/BaterryPickup.cpp
@@ -0,0 +1,13 @@
+
+
+#include "TutorialCode.h"
+#include "BaterryPickup.h"
+
+
+ABaterryPickup::ABaterryPickup(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/TutorialCode/Public/BaterryPickup.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "Pickup.h"
+#include "BaterryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUTORIALCODE_API ABaterryPickup : public APickup
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
